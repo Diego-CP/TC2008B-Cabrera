677ca5388fde07af26fc557ed278a3dfcd163c1b
Diego-CP <pinedo.dc@gmail.com>
2022-11-22 13:24:21 -0600

    Minor aesthetic changes

--- Integradora1/TrafficVisualization/Assets/Scripts/AgentController.cs
+++ Integradora1/TrafficVisualization/Assets/Scripts/AgentController.cs
@@ -95,7 +95,7 @@ public class AgentController : MonoBehaviour
                 Vector3 direction = currentPosition - interpolated;
 
                 agents[agent.Key].transform.localPosition = interpolated;
-                if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(new Vector3(-direction.x, direction.y, -direction.z));
             }
 
             // float t = (timer / timeToUpdate);
